ad3d7be4db2d0c5076b5d1feefee114db1e7c0cf
milanesjonata <milanes.jonata@gmail.com>
2025-11-25 20:29:14 -0300

    v01-criando as tabelas

---
+++ 01_create_tables.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE IF NOT EXISTS estoque;
+USE estoque;
+
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome_categoria VARCHAR(100) NOT NULL,
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE fornecedor (
+    id_fornecedor INT AUTO_INCREMENT PRIMARY KEY,
+    nome_fornecedor VARCHAR(100) NOT NULL,
+    contato VARCHAR(100),
+    email VARCHAR(100),
+    cnpj VARCHAR(18)
+);
+
+CREATE TABLE produto (
+    id_poroduto INT AUTO_INCREMENT PRIMARY KEY,
+    id_categoria INT NOT NULL,
+    id_fornecedor INT NOT NULL,
+    nome_produto VARCHAR(100) NOT NULL,
+    unidade VARCHAR(20),
+    quantidade_minima INT DEFAULT 0,
+
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
+    FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id_fornecedor)
+);
+
+CREATE TABLE lote (
+    id_estoque INT AUTO_INCREMENT PRIMARY KEY,
+    id_poroduto INT NOT NULL,
+    id_fornecedor INT NOT NULL,
+    numero_lote VARCHAR(40) NOT NULL,
+    data_validade DATE,
+    quantidade INT NOT NULL,
+
+    FOREIGN KEY (id_poroduto) REFERENCES produto(id_poroduto),
+    FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id_fornecedor)
+);  
+
+CREATE TABLE responsavel (
+    id_responsavel INT AUTO_INCREMENT PRIMARY KEY,
+    nome_responsavel VARCHAR(100) NOT NULL,
+    cargo VARCHAR(100),
+    telefone VARCHAR(15),
+    email VARCHAR(100)
+);
+
+CREATE TABLE movimentacao (
+    id_movimentacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_responsavel INT NOT NULL,
+    id_lote INT NOT NULL,
+    tipo_movimentacao ENUM('entrada', 'saida') NOT NULL,
+    quantidade INT NOT NULL,
+    data_movimentacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_responsavel) REFERENCES responsavel(id_responsavel),
+    FOREIGN KEY (id_lote) REFERENCES lote(id_lote)
+);
